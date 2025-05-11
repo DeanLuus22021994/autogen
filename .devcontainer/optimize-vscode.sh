775e794166638394fac56ff20458fe2f1a2eab1d
DeanLuus22021994 <dean.luus22021994@gmail.com>
2025-05-11 17:18:17 +0200

    Remove deprecated GPU detection and optimization scripts from devcontainer

--- .devcontainer/optimize-vscode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Optimizing VS Code Server performance..."
-if [ -d /root/.vscode-server ]; then
-    find /root/.vscode-server -name "*.js" -type f -exec touch {} \;
-    find /root/.vscode-server -name "*.json" -type f -exec touch {} \;
-fi
